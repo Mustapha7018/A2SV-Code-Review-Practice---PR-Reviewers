4b6747af940ddb7aad7243adebd699caf6d38c4e
Mustapha7018 <adamsmustapha419@gmail.com>
2024-04-04 15:28:41 +0000

    initial commit

---
+++ task-scheduler.dart
@@ -0,0 +1,53 @@
+class Task {
+
+  // Properties
+ 
+
+  // Task constructor
+  Task();
+  
+}
+
+class TaskScheduler {
+
+  // Add task method
+  void addTask() {
+
+  }
+
+
+  // View all tasks
+  void viewAllTask() {
+
+  }
+
+
+  // View all completed tasks
+  void viewCompletedTasks() {
+
+  }
+
+
+  // View all pending tasks
+  void viewPendingTasks() {
+
+  }
+
+
+  // Edit task method
+  void editTask() {
+
+  }
+
+
+  // Delete task
+  void deleteTask() {
+
+  }
+
+}
+
+
+void main() {
+  
+}
