5961ae28aa0c0861d957a3a63aebba8cef534372
Farad Wells <alwaasiua@gmail.com>
2024-04-04 16:54:42 +0000

    Create a task class constructor

--- Task.dart
+++ Task.dart
@@ -4,4 +4,13 @@ class Task{
   String? _status;
   DateTime? _dueDate;
 
+    Task(String? title, String? description, String? status, DateTime? dueDate){
+    this._title = title;
+    this._description = description;
+    this._status = status;
+    this._dueDate = dueDate;
+  }
+
+  
+
 }
