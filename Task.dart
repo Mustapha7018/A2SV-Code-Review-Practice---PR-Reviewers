997bbf7d4438db10e29b50759dae4553337dcfe5
Farad Wells <alwaasiua@gmail.com>
2024-04-04 17:01:42 +0000

    Create Task Class getter and setter functions

--- Task.dart
+++ Task.dart
@@ -10,6 +10,38 @@ class Task{
     this._status = status;
     this._dueDate = dueDate;
   }
+Create Task class setter and getter functions
+  void setTitle(String title) {
+    this._title = title;
+  }
+
+  void setDescription(String description) {
+    this._description = description;
+  }
+
+  void setDate(DateTime dueDate) {
+    this._dueDate = dueDate;
+  }
+
+  void setStatus(String? status) {
+    this._status = status;
+  }
+
+  String? getTitle() {
+    return this._title;
+  }
+
+  String? getDescription() {
+    return this._description;
+  }
+
+  DateTime? getDate() {
+    return this._dueDate;
+  }
+
+  String? getStatus() {
+    return this._status;
+  }
 
 
 }
